823390b0f5b829711356829071af7d6c23d409d5
pjames <pjames@gmail.com>
2025-11-17 14:21:01 +0700

    Update vite.config.ts to allow all host

--- cycop1/frontend/vite.config.ts
+++ cycop1/frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: true,       // ฟังทุก IP
+    port: 5173,
+    allowedHosts: "all",  // อนุญาตทุก host
+  },
 })
