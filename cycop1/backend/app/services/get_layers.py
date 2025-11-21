93d4f3b38f56b9eb7ab16d9298a75f8b2572aa12
pjames <pjames@gmail.com>
2025-11-21 20:49:15 +0700

    Update env vars

--- cycop1/backend/app/services/get_layers.py
+++ cycop1/backend/app/services/get_layers.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 # โหลดค่า .env
 load_dotenv(".env")
 
-API_PATH = os.getenv("API_HTTP_ENDPOINT")
-API_KEY = os.getenv("API_KEY")
-ORG_ID = os.getenv("ORG_ID")
+API_PATH = os.getenv("API_PATH")
+API_KEY = os.getenv("API_AUTHEN_PASSWORD")
+ORG_ID = "default"
 print("API_PATH = ", API_PATH)
 print("API_KEY = ", API_KEY)
 print("ORG_ID = ", ORG_ID)
