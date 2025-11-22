d9b96fdd5c52b2fb0f6757e7a38b37b2a6ea3107
auttz <auttzmargaretz@gmail.com>
2025-11-22 12:32:19 +0700

    fix get_layers.py

--- cycop1/backend/app/services/get_layers.py
+++ cycop1/backend/app/services/get_layers.py
@@ -21,7 +21,7 @@ def get_all_layers():
 
     response = requests.get(
         url,
-        auth=HTTPBasicAuth("api", API_KEY)
+        auth=HTTPBasicAuth(ORG_ID, API_KEY)
     )
 
     print("Status code:", response.status_code)
